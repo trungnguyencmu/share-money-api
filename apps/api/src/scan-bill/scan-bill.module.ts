3a689645e99c8c3e5ead8ed43ad90ddd660b3507
Trung Nguyen <trung.nguyen@oivan.com>
2026-03-24 11:23:01 +0700

    fix: import StorageModule into ScanBillModule for S3Service

--- apps/api/src/scan-bill/scan-bill.module.ts
+++ apps/api/src/scan-bill/scan-bill.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { StorageModule } from '../storage/storage.module';
 import { TripsModule } from '../trips/trips.module';
 import { ScanBillController } from './scan-bill.controller';
 import { ScanBillService } from './scan-bill.service';
 
 @Module({
-  imports: [DatabaseModule, TripsModule],
+  imports: [DatabaseModule, TripsModule, StorageModule],
   controllers: [ScanBillController],
   providers: [ScanBillService],
   exports: [ScanBillService],
